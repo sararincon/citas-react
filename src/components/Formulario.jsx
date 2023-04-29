58d1dd4f886cfc5b370b95663d5d11c5c19a75f4
Sara Rincon <sararincon@MacBook-Air-de-Sara.local>
2023-04-28 22:02:54 -0400

    WIP

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,11 +9,31 @@ const Formulario = () => {
                Añade Paciente y {""} <span className="text-indigo-600 font-bold">Administralos</span>
            </p>
 
-           <form className="mt-5 bg-white">
+           <form className="mb-5 bg-white rounded-lg shadow-md  py-5 px-5">
 
-               <div className="bg-white shadow-md rounded-lg py-5 px-5">
-                   <label className="block">Nombre Mascota: {""}</label>
-                   <input type= "text" placeholder="Nombre"/>
+               <div className="mb-3">
+                   <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold ">Nombre Mascota: {""}</label>
+                   <input  id= "mascota" className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type= "text" placeholder="Nombre"/>
+               </div>
+
+               <div className="mb-3">
+                   <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold ">Nombre del Propietario: {""}</label>
+                   <input  id= "propietario" className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type= "text" placeholder="Nombre del Propietario"/>
+               </div>
+
+               <div className="mb-3">
+                   <label htmlFor="email" className="block text-gray-700 uppercase font-bold ">Email: {""}</label>
+                   <input  id= "email" className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type= "email" placeholder="Email"/>
+               </div>
+
+               <div className="mb-3">
+                   <label htmlFor="alta" className="block text-gray-700 uppercase font-bold ">Fecha de Alta:{""}</label>
+                   <input  id= "alta" className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type= "date" />
+               </div>
+
+               <div className="mb-3">
+                   <label htmlFor="alta" className="block text-gray-700 uppercase font-bold ">Fecha de Alta:{""}</label>
+                   <textarea className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas"/>
                </div>
 
            </form>
